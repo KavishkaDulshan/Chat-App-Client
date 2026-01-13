19933bc90ca90d1aa919ca50b7f9a65865a4756c
PDK Dulshan <pdkdulshan@students.nsbm.ac.lk>
2026-01-13 16:12:35 +0530

    HTTPS setup for security with server side Nginx

--- lib/config.dart
+++ lib/config.dart
@@ -3,7 +3,7 @@ import 'package:flutter/foundation.dart'; // <--- USE THIS, NOT dart:io
 class AppConfig {
   static const bool isProduction = true;
   static const String productionUrl =
-      'https://chat-app-backend-zqik.onrender.com';
+      'https://blinkchat.uaenorth.cloudapp.azure.com';
 
   static String get baseUrl {
     if (isProduction) {
