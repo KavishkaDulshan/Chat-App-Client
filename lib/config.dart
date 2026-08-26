213817330c9d386160fab42b128a4fbb380acc37
Kavishka Dulshan <kavishkadulshan2@gmail.com>
2026-08-26 11:00:06 +0530

    fix:material widget warning fix

--- lib/config.dart
+++ lib/config.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/foundation.dart'; // <--- USE THIS, NOT dart:io
 
 class AppConfig {
-  static const bool isProduction = false;
+  static const bool isProduction = true;
   static const String productionUrl =
       'http://blinkchat.uaenorth.cloudapp.azure.com';
 
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -326,7 +326,9 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
         borderRadius: BorderRadius.circular(12),
         border: Border.all(color: const Color(0xFFE2E8F0), width: 1),
       ),
-      child: ListTile(
+      child: Material(
+        color: AppTheme.cardColor,
+        child: ListTile(
         contentPadding:
             const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
         leading: Stack(
@@ -381,6 +383,7 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
             : null,
         onTap: () => _joinChat(conv),
       ),
+      )
     );
   }
 
